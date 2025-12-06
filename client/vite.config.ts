2b28d2dc60f17ed2ecfc615ebf8475f101407e89
Vitaly Frolov <vifrolov@rdcenter.ru>
2025-12-06 04:50:38 +0300

    feat(DOCKER): client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,28 +1,37 @@
+import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://95.174.104.37:4200', // порт виталика
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@features': path.resolve(__dirname, './src/features'),
-      '@entities': path.resolve(__dirname, './src/entities'),
-      '@shared': path.resolve(__dirname, './src/shared'),
-      '@widgets': path.resolve(__dirname, './src/widgets'),
-    },
-  },
+	plugins: [react()],
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://95.174.104.37:4200', // порт виталика
+				changeOrigin: true,
+				secure: false,
+			},
+		},
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@pages': path.resolve(__dirname, './src/pages'),
+			'@features': path.resolve(__dirname, './src/features'),
+			'@entities': path.resolve(__dirname, './src/entities'),
+			'@shared': path.resolve(__dirname, './src/shared'),
+			'@widgets': path.resolve(__dirname, './src/widgets'),
+		},
+	},
+	build: {
+		chunkSizeWarningLimit: 800,
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					react: ['react', 'react-dom'],
+					vendor: ['axios', 'zustand'],
+				},
+			},
+		},
+	},
 });
-
-
